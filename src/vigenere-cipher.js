9415a51cc08c30539b530009c8dd6c7cfdb62d46
Alexandr <k.v.alexandr@gmail.com>
2020-10-07 10:22:36 +0400

    feat: add vigenere cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type = true) {
+    this.type = type;
+  }
+
+  model(message, key, fn) {
+    if (!message || !key) throw new Error();
+
+    message = this.type ? message : message.split('').reverse().join('');
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = '';
+    let keyCounter = 0;
+
+    for (let i = 0; i < message.length; i += 1) {
+      if (/^[A-Z]$/.test(message[i])) {
+        const messageCharCode = message.charCodeAt(i);
+        const keyCharCode = key.charCodeAt(keyCounter);
+        const resultCode = fn(messageCharCode, keyCharCode);
+
+        keyCounter = (keyCounter + 1) % key.length;
+
+        result += String.fromCharCode(resultCode);
+      } else {
+        result += message[i];
+      }
+    }
+
+    return result;
+  }
+
+  encrypt(message, key) {
+    return this.model(message, key, (m, k) => (m + k) % 26 + 65);
+  }
+
+  decrypt(message, key) {
+    return this.model(message, key, (m, k) => (m + 26 - k) % 26 + 65);
   }
 }
 
